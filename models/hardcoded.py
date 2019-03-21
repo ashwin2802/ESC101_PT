b1da80130fd055c76d5c427ef188cc2e036fd802
Ashwin Shenai <ashwins@iitk.ac.in>
2019-03-21 07:24:30 +0530

    model can now get ball data

--- models/hardcoded.py
+++ models/hardcoded.py
@@ -1,8 +1,27 @@
 from app import comp
+from app import pong
+import os.path as path
+
+
+def get_ball_pos():
+    filepath = path.join(path.dirname(path.dirname(
+        path.abspath(__file__))), 'data/ball_pos.txt')
+    with open(filepath, 'r') as f:
+        data = f.readline()
+        f.close()
+    [x, y, angle, v] = data.split(',')
+    # pong.debug(data)
+    return int(x), int(y), int(angle), float(v)
+
+
+def make_prediction(x, y, angle, v):
+    pred = (x/x) - (y/y) + (angle/angle) + (v/v)
+    # pong.debug(pred)
+    return pred
 
 
 def get_prediction():
-    return 1
-
-
-print("Hi")
+    x, y, angle, v = get_ball_pos()
+    prediction = make_prediction(x, y, angle, v)
+    # prediction =
+    return prediction
